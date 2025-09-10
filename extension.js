cf19eed0b31a0ac354e67c61389df6281462c876
skandinaff <sevalks@gmail.com>
2025-09-10 18:30:24 +0300

    added initial logic

--- extension.js
+++ extension.js
@@ -18,6 +18,7 @@
 
 import GObject from 'gi://GObject';
 import St from 'gi://St';
+import GLib from 'gi://GLib';
 
 import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
@@ -25,32 +26,46 @@ import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
-const Indicator = GObject.registerClass(
-class Indicator extends PanelMenu.Button {
+// Simple indicator with a menu for switching inputs via ddcutil
+const DisplaySwitchIndicator = GObject.registerClass(
+class DisplaySwitchIndicator extends PanelMenu.Button {
     _init() {
-        super._init(0.0, _('My Shiny Indicator'));
+        super._init(0.0, _('Display Switch'));
 
+        // Panel icon
         this.add_child(new St.Icon({
-            icon_name: 'face-smile-symbolic',
+            icon_name: 'video-display-symbolic',
             style_class: 'system-status-icon',
         }));
 
-        let item = new PopupMenu.PopupMenuItem(_('Show Notification'));
-        item.connect('activate', () => {
-            Main.notify(_('Whatʼs up, folks?'));
+        // HDMI-1 action
+        const hdmiItem = new PopupMenu.PopupMenuItem(_('Switch to HDMI-1'));
+        hdmiItem.connect('activate', () => {
+            // VCP code 0x60 (Input Select), value 0x11 (HDMI-1)
+            GLib.spawn_command_line_async('ddcutil setvcp 60 0x11');
         });
-        this.menu.addMenuItem(item);
+        this.menu.addMenuItem(hdmiItem);
+
+        // DisplayPort-1 action
+        const dpItem = new PopupMenu.PopupMenuItem(_('Switch to DisplayPort-1'));
+        dpItem.connect('activate', () => {
+            // VCP code 0x60 (Input Select), value 0x0f (DisplayPort-1)
+            GLib.spawn_command_line_async('ddcutil setvcp 60 0x0f');
+        });
+        this.menu.addMenuItem(dpItem);
     }
 });
 
-export default class IndicatorExampleExtension extends Extension {
+export default class DisplaySwitchExtension extends Extension {
     enable() {
-        this._indicator = new Indicator();
+        this._indicator = new DisplaySwitchIndicator();
         Main.panel.addToStatusArea(this.uuid, this._indicator);
     }
 
     disable() {
-        this._indicator.destroy();
-        this._indicator = null;
+        if (this._indicator) {
+            this._indicator.destroy();
+            this._indicator = null;
+        }
     }
 }
